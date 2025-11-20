230bd66d19da47ceb46ffc5329513dad6be6b01f
이용민 <bacaren21@naver.com>
2025-11-21 02:17:22 +0900

    ListGraph 작성

--- ListGraph.cpp
+++ ListGraph.cpp
@@ -4,30 +4,81 @@
 
 ListGraph::ListGraph(bool type, int size) : Graph(type, size)
 {
-	
+    // adjacency list 생성
+    m_List = new map<int, int>[size];
 }
 
-ListGraph::~ListGraph()	
+ListGraph::~ListGraph()
 {
-	
+    // 동적 배열 해제
+    delete[] m_List;
 }
 
-void ListGraph::getAdjacentEdges(int vertex, map<int, int>* m)	 //Definition of getAdjacentEdges(No Direction == Undirected)
+void ListGraph::getAdjacentEdges(int vertex, map<int, int> *m) // Definition of getAdjacentEdges(No Direction == Undirected)
 {
-	
+    // 1. 정방향 간선 추가
+    for (auto &cur_vertex : m_List[vertex])
+    {
+        int to = cur_vertex.first;
+        int weight = cur_vertex.second;
+        (*m)[to] = weight;
+    }
+
+    // 2. 역방향 간선도 찾아서 추가 (무방향 그래프에서 인접 정점을 구하기 위해)
+    for (int i = 0; i < m_Size; i++)
+    {
+        if (m_List[i].find(vertex) != m_List[i].end())
+        {
+            (*m)[i] = m_List[i][vertex];
+        }
+    }
 }
 
-void ListGraph::getAdjacentEdgesDirect(int vertex, map<int, int>* m)	//Definition of getAdjacentEdges(Directed graph)
+void ListGraph::getAdjacentEdgesDirect(int vertex, map<int, int> *m) // Definition of getAdjacentEdges(Directed graph)
 {
-	
+    // Vertex에서 나가는 정방향 간선을 추가
+    for (auto &cur_vertex : m_List[vertex])
+    {
+        (*m)[cur_vertex.first] = cur_vertex.second;
+    }
 }
 
-void ListGraph::insertEdge(int from, int to, int weight) //Definition of insertEdge
+void ListGraph::insertEdge(int from, int to, int weight) // Definition of insertEdge
 {
-	
+    m_List[from][to] = weight;
 }
 
-bool ListGraph::printGraph(ofstream *fout)	//Definition of print Graph
+bool ListGraph::printGraph(ofstream *fout) // Definition of print Graph
 {
-	
+    if (!fout || !fout->is_open())
+    {
+        return false;
+    }
+
+    for (int i = 0; i < m_Size; i++)
+    {
+        (*fout) << "[" << i << "]";
+
+        // 아무 간선도 없으면 바로 줄바꿈
+        if (m_List[i].empty())
+        {
+            (*fout) << " ->" << "\n";
+            continue;
+        }
+
+        // map<int,int> 자동으로 key(도착정점) 오름차순 정렬
+        for (auto &edge : m_List[i])
+        {
+            int to = edge.first;
+            int weight = edge.second;
+
+            (*fout) << " -> (" << to << "," << weight << ")";
+        }
+
+        (*fout) << "\n";
+    }
+
+    (*fout) << "\n"; // 마지막 줄 공백 한 줄
+
+    return true;
 }
--- ListGraph.h
+++ ListGraph.h
@@ -3,20 +3,20 @@
 
 #include "Graph.h"
 
-class ListGraph : public Graph{	
+class ListGraph : public Graph
+{
 private:
-	map < int, int >* m_List;
-	vector<int>* kw_graph;
-	
-public:	
+	map<int, int> *m_List; // (도착점, 가중치)를 저장하는 map을 저장하는 배열을 가리키는 포인터
+	vector<int> *kw_graph;
+
+public:
 	ListGraph(bool type, int size);
 	~ListGraph();
-		
-	void getAdjacentEdges(int vertex, map<int, int>* m);	
-	void getAdjacentEdgesDirect(int vertex, map<int, int>* m);
-	void insertEdge(int from, int to, int weight);	
-	bool printGraph(ofstream *fout);
 
+	void getAdjacentEdges(int vertex, map<int, int> *m);
+	void getAdjacentEdgesDirect(int vertex, map<int, int> *m);
+	void insertEdge(int from, int to, int weight);
+	bool printGraph(ofstream *fout);
 };
 
 #endif
